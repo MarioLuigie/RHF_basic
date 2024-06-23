b7905ba7c623d4068c3c58f91eeeb70128ccdef0
Mariusz Łotocki <mk.lotocki@gmail.com>
2024-06-24 00:52:29 +0200

    Feat: Register inputs to useForm

--- src/components/forms/SignInForm.tsx
+++ src/components/forms/SignInForm.tsx
@@ -3,26 +3,37 @@
 import Input from '@/components/shared/Input'
 import Button from '@/components/shared/Button'
 
+import { useForm, SubmitHandler } from 'react-hook-form'
+
+// Określenie typu dla pól/inputów formularza
+type FormFields = {
+	email: string
+	password: string
+}
+
 export default function SignInForm() {
-	const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-		e.preventDefault()
-		console.log('Submitted successfully!')
+	// Zbudowanie instancji obiektu formularza z react hook form 'const form = useForm<FormFields>()'
+	// Podłączenie pól.inputów do form za pomocą register, register przyjmuje name dla inputa
+	const { register, handleSubmit } = useForm<FormFields>()
+
+	const onSubmit: SubmitHandler<FormFields> = (data) => {
+		console.log(data);
 	}
 
 	return (
-		<form onSubmit={handleSubmit}>
+		<form onSubmit={handleSubmit(onSubmit)}>
 			<div className="flex flex-col gap-5 items-end justify-center p-10">
 				<Input
+					register={register('email')}
 					type="text"
 					placeholder="Email"
 					label="Email"
-					isRequired={true}
 				/>
 				<Input
+					register={register('password')}
 					type="password"
 					placeholder="Password"
 					label="Password"
-					isRequired={true}
 				/>
 			</div>
 			<Button label="Submit" />
--- src/components/shared/Input.tsx
+++ src/components/shared/Input.tsx
@@ -1,22 +1,24 @@
+import { UseFormRegisterReturn } from 'react-hook-form';
+
 export default function Input({
 	type,
 	placeholder,
 	label,
-	isRequired,
+	register,
 }: {
 	type: string
 	placeholder: string
 	label: string
-	isRequired: boolean
+	register: UseFormRegisterReturn
 }) {
 	return (
 		<div className="gap-4 flex items-center justify-end w-full">
 			<label className="w-[100px] flex justify-end">{label}</label>
 			<input
+				{...register}
 				type={type}
 				placeholder={placeholder}
 				className="p-2 pl-4 border border-gray-800 rounded-[50px] w-full"
-				required={isRequired}
 			/>
 		</div>
 	)
