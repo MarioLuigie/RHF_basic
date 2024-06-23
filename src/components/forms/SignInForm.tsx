b4a958c878e5d11d6ee18e62467e32f1678c92ab
Mariusz Łotocki <mk.lotocki@gmail.com>
2024-06-23 23:25:32 +0200

    Feat: Add required for inputs

--- src/components/forms/SignInForm.tsx
+++ src/components/forms/SignInForm.tsx
@@ -4,13 +4,17 @@ import Input from '@/components/shared/Input'
 import Button from '@/components/shared/Button'
 
 export default function SignInForm() {
-	const handleSubmit = () => {}
+	
+	const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault()
+		console.log('Submitted successfully!')
+	}
 
 	return (
 		<form onSubmit={handleSubmit}>
 			<div className='flex flex-col gap-5 items-end justify-center p-10'>
-				<Input type="text" placeholder="Email" label="Email"/>
-				<Input type="password" placeholder="Password" label="Password"/>
+				<Input type="text" placeholder="Email" label="Email" isRequired={true} />
+				<Input type="password" placeholder="Password" label="Password" isRequired={true} />
 			</div>
 			<Button label='Submit'/>
 		</form>
--- src/components/shared/Input.tsx
+++ src/components/shared/Input.tsx
@@ -2,15 +2,22 @@ export default function Input({
 	type,
 	placeholder,
 	label,
+	isRequired,
 }: {
 	type: string
 	placeholder: string
 	label: string
+	isRequired: boolean
 }) {
 	return (
 		<div className="gap-4 flex items-center justify-end w-full">
 			<label className="w-[100px] flex justify-end">{label}</label>
-			<input type={type} placeholder={placeholder} className="p-2 pl-4 border border-gray-800 rounded-[50px] w-full"/>
+			<input
+				type={type}
+				placeholder={placeholder}
+				className="p-2 pl-4 border border-gray-800 rounded-[50px] w-full"
+				required={isRequired}
+			/>
 		</div>
 	)
 }
